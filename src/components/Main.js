055fabcf4a3d2549732911c50224e41b6d0c48bf
OhMedrano <Omedrano564@gmail.com>
2016-12-19 01:25:55 -0500

    Added comments into main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -85,20 +85,99 @@ class AppComponent extends React.Component {
 
 					But what if the location data is old? 
 
+	
+					We'll want to update that right? 
+
+					So we'll use the "date" variable declared above at 
+
+					line 59, and compare the minutes. 
+
+
+
 				*/
 
 				console.log(date.getMinutes());	
 
+				/*
+					If the minutes are divisible by 10 is what this
+
+					if statement is declaring. 
+
+				*/
 				if(date.getMinutes()%10 === 0 ){
 					console.log('updated the location');
+
+						/*
+							Next we're going to check if the browser 
+
+							supports geolocation, if it does then we'll update it.
+
+
+
+							NOTE: Planning to clean this up. Too much repeatable code.
+
+							Can make a simple function for this. 
+
+						*/
 
 					if(window.navigator && window.navigator.geolocation){
 						window.navigator.geolocation.getCurrentPosition(function(position){
 							console.log('updating the location right now');
 							
+							/*
+								If geolocation is active,
+
+								then we'll get the location date which is now stored in the 
+
+								variable called "position". 
+
+
+								If you console.log(position), you'll see all the data it contains. 
+
+
+
+							*/
+
 							localStorage.setItem('localLat',parseFloat(position.coords.latitude).toFixed(2));
 							localStorage.setItem('localLon',parseFloat(position.coords.longitude).toFixed(2));
-						
+							
+							/*
+								Here we are storing the position coordinate data,
+
+								particularly the latitude and longitude. 
+
+
+								However, you can see I added a parseFloat() on there,.
+
+								The reason for that is, when you're calling that data, 
+
+								it will be coming back into a string. 
+
+
+								That's cool and all, but I want to manipulate it before 
+
+								it gets stored into the localStorage. 
+
+
+								Hence the parseFloat. It changes the data from a string to
+
+								a floating point number aka a number with a decimal point. 
+
+								From there, added the toFixed() method, so that way instead of
+
+								getting something like 12.3456789. I can call in the toFixed(2), 
+
+								and transform the data to 12.34 . 
+
+
+								This distinction is vital for the usage of the openweather API, 
+
+								it only accepts values up to the hundredth's (hundredths?) place. 
+
+
+
+
+							*/
 
 							lon = localStorage.getItem('localLon');
 							lat = localStorage.getItem('localLat');
@@ -119,6 +198,13 @@ class AppComponent extends React.Component {
 				} else {
 					console.log('didn"t update');
 
+					/*
+						If the minutes weren't disivible by 10, 
+
+						then we'll just use the value in the localStorage.
+
+					*/
+
 					lon = localStorage.getItem('localLon');
 					lat = localStorage.getItem('localLat');
 
@@ -135,6 +221,22 @@ class AppComponent extends React.Component {
 
 				console.log('Got the local storage data!');
 			} else {
+
+				/*
+
+					This part is the same as above except 
+
+					this is what triggers when it doesn't find anything
+
+					in localStorage's databank.
+
+
+					NOTE: I will be planning to refactor this and change this into 
+
+					function that I can call. 
+
+				*/
+
 				if(window.navigator && window.navigator.geolocation){
 					window.navigator.geolocation.getCurrentPosition(function(position){
 						console.log(position);
@@ -174,6 +276,10 @@ class AppComponent extends React.Component {
 				}
 			}
 		} 
+	}
+
+	compoentDidMount(){
+
 	}
 
 
